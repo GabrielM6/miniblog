33bfe281afacc14b288ba8102d2c2bc4bc8c55ee
Gabriel Moreno <gabrielm37000@gmail.com>
2024-10-07 11:45:50 -0300

    subindo o projeto todo

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfhJ7vVy3FO__Z12nu5R8YcVsKHb76B0s",
+  authDomain: "miniblog-956e0.firebaseapp.com",
+  projectId: "miniblog-956e0",
+  storageBucket: "miniblog-956e0.appspot.com",
+  messagingSenderId: "697969107667",
+  appId: "1:697969107667:web:7d0d13d92a9dfadc5a6a26"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export {db}
