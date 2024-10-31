18d0543dc37b3525ad5e3021e8250d5b92f11d0c
Gabriel Moreno <gabrielm37000@gmail.com>
2024-10-31 14:58:02 -0300

    Subindo projeto completo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,48 @@
+import { useState } from 'react'
 import styles from './Home.module.css'
+import { useNavigate, Link, NavLink, Navigate } from 'react-router-dom'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import PostDetail from '../../components/PostDetail'
+
 
 
 const Home = () => {
+  const [query, setQuery] = useState('')
+  const {documents: posts, loading} = useFetchDocuments('posts')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    if(query){
+      return navigate(`/search?q=${query}`)
+    }
+
+  }
+
+
   return (
-    <div>
+    <div className={styles.home}>
       <h1>
-        Home
+        Veja os nossos posts mais recentes
       </h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input type="text" placeholder='Ou busque por tags...' onChange={(e) => setQuery(e.target.value) }/>
+        <button className='btn btn-dark'>Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => (
+          <PostDetail post={post} key={post.id}/>
+        ))}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to='/posts/create' className='btn'>Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
